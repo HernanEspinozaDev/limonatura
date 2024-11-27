36c0b65df64740e21ed2928d4cd0fc117dde6837
Hernán Espinoza <hernan.espinoza.dev@gmail.com>
2024-11-27 15:35:08 -0300

    allowed host all permitidos

--- limonatura/settings.py
+++ limonatura/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-ceh5e7553jc^)#he&ky)n-l784!+@rcjio+-czc=u7%oe!%men
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
